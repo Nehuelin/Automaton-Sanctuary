c186f5284a62fe3df6143d2fc233094bb676321a
Nehuelin <nehuel2005@gmail.com>
2025-08-22 09:23:54 -0300

    reset NFA code, still pending implementation

--- src/NFA.java
+++ src/NFA.java
@@ -26,68 +26,7 @@ public class NFA extends FiniteAutomaton{
 
     @Override
     public boolean accept(String input) {
-        return acceptHelper(startState, input, 0);
-    }
-
-    private boolean acceptHelper(String currentState, String input, int position){
-
-        if(position == input.length()){
-            HashSet<String> reachableStates = getEpsilonClosure(currentState);
-            return reachableStates.stream().anyMatch(s -> acceptStates.contains(s));
-        }
-
-        char symbol = input.charAt(position);
-
-        if(!alphabet.contains(symbol)){
-            return false;
-        }
-
-        HashSet<String> epsilonStates = getEpsilonClosure(currentState);
-
-        for(String state : epsilonStates){
-            HashSet<String> nextStates = getTransitions(state, symbol);
-
-            for(String nextState : nextStates){
-                if(acceptHelper(nextState, input, position + 1)){
-                    return true;
-                };
-
-            }
-        }
-
-
         return false;
     }
 
-    private HashSet<String> getEpsilonClosure(String state){
-        HashSet<String> closure = new HashSet<>();
-        computeEpsilonClosure(state, closure);
-        return closure;
-    }
-
-    private void computeEpsilonClosure(String state, HashSet<String> visited){
-        if (!visited.add(state)){
-            return;
-        }
-
-        for (Transition t : delta) {
-            if (t.getFromState().equals(state) && t.getSymbol() == EPSILON) {
-                computeEpsilonClosure(t.getToState(), visited);
-            }
-        }
-    }
-
-    private HashSet<String> getTransitions(String state, char symbol){
-        HashSet<String> nextStates = new HashSet<>();
-        for(Transition t : delta){
-            if(t.getFromState().equals(state) && t.getSymbol() == symbol){
-                nextStates.add(t.getToState());
-            }
-        }
-        return nextStates;
-    }
-
-    public DFA transformToDFA(){
-        return null;
-    }
 }
